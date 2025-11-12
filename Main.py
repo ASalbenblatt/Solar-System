312963c02b4b578bdd141cd17232c9d040c8db27
ASalbenblatt <ausa6999@colorado.edu>
2025-11-11 20:16:07 -0700

    Blank window is made

--- Main.py
+++ Main.py
@@ -1,6 +1,33 @@
+''' controls:
+q - quit
+
+'''
+
 from math import *
+from functions import *
 
 import pygame
 
 import random as rnd
 import numpy as np
+
+screen = pygame.display.set_mode()
+
+timer = pygame.time.Clock()
+
+while(True):
+    timer.tick(60)        #Sets the framerate to 60
+    pygame.event.pump()   #keeps key inputs working
+
+    keys = pygame.key.get_pressed()
+
+    screen.fill([0, 0, 5])
+
+
+
+    pygame.display.flip()  #updates the screen
+
+    #hitting 'q' will quit
+    if keys[pygame.K_q]:
+        pygame.display.quit()
+        break
