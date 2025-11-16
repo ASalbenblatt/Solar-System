a1f869537ce421d735f6636e73df8a3e539c6403
ASalbenblatt <ausa6999@colorado.edu>
2025-11-16 12:15:27 -0700

    Fixed controls and added new controls like arrows and camera

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 ''' controls:
 q - quit
 click and drag - new planet
+a - toggle acc arrows
+r - reset
+m - toggle center of mass
+c - toggle the camera
+space and drag - pan the camera
 '''
 
 from math import *
@@ -29,15 +34,22 @@ mass_options = [1, 5, 10, 25, 50, 100, 250]
 mass_selection = 3
 
 camera = np.array([0, 0])
+camera_on_COM = False
+panning = False
+pre_pan_diff = None
+
+COM = np.array([0, 0])
+COM_toggle = False
+COM_size = 10
+COM_width = 2
 
 to_delete = []
 
 while(True):
     timer.tick(60)        #Sets the framerate to 60
-    pygame.event.pump()   #keeps key inputs working
 
     keys = pygame.key.get_pressed()
-    event = pygame.event.poll()
+    events = pygame.event.get()
 
     screen.fill([0, 0, 5])
 
@@ -60,26 +72,62 @@ while(True):
     for i in planets:
         i.draw(screen, camera)
 
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_PERIOD:
-        mass_selection = (mass_selection + 1) % 7
-    
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_COMMA:
-        mass_selection = (mass_selection - 1) % 7
+    if len(planets) != 0:
+        COM  = np.array([0, 0])
+        total_mass = 0
+        for i in planets:
+            COM = COM + (i.coords * i.mass)
+            total_mass = total_mass + i.mass
+        COM = COM/total_mass
+    if COM_toggle:
+        pygame.draw.line(screen, (60, 60, 225), (COM[0] - camera[0] - COM_size, COM[1] - camera[1]), (COM[0] - camera[0] + COM_size, COM[1] - camera[1]), COM_width)
+        pygame.draw.line(screen, (60, 60, 225), (COM[0] - camera[0], COM[1] - camera[1] - COM_size), (COM[0] - camera[0], COM[1] - camera[1] + COM_size), COM_width)
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        dragging = True
-        pre_drag_pos = np.array(pygame.mouse.get_pos())
-
-    if event.type == pygame.MOUSEBUTTONUP and dragging == True:
-        dragging = False
-        planets.append(planet(mass_options[mass_selection], pre_drag_pos + camera, pre_drag_pos - np.array(pygame.mouse.get_pos()), (rand.random()*200 + 25, rand.random()*200 + 25, rand.random()*200 + 25)))
+    if camera_on_COM:
+        camera = COM - np.array([pygame.Surface.get_width(screen)/2, pygame.Surface.get_height(screen)/2])
 
     if dragging:
         draw_arrow(screen, pygame.math.Vector2(pygame.mouse.get_pos()), pygame.math.Vector2(pre_drag_pos[0], pre_drag_pos[1]), pygame.Color(100, 100, 100), 5, 15, 15)
 
+    if keys[pygame.K_SPACE] and not camera_on_COM:
+            camera = pre_pan_diff - np.array(pygame.mouse.get_pos())
+
     pygame.display.flip()  #updates the screen
 
-    #hitting 'q' will quit
-    if keys[pygame.K_q] or event.type == pygame.QUIT:
-        pygame.display.quit()
-        break
+    for event in events:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            pre_pan_diff = camera + np.array(pygame.mouse.get_pos())
+    
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_PERIOD:
+            mass_selection = (mass_selection + 1) % 7
+        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_COMMA:
+            mass_selection = (mass_selection - 1) % 7
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+            toggle_acc()
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_v:
+            toggle_vel()
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
+            camera_on_COM = not camera_on_COM
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            planets = []
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            dragging = True
+            pre_drag_pos = np.array(pygame.mouse.get_pos())
+
+        if event.type == pygame.MOUSEBUTTONUP and dragging == True:
+            dragging = False
+            planets.append(planet(mass_options[mass_selection], pre_drag_pos + camera, pre_drag_pos - np.array(pygame.mouse.get_pos()), (rand.random()*200 + 25, rand.random()*200 + 25, rand.random()*200 + 25)))
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
+            COM_toggle = not COM_toggle
+
+        #hitting 'q' will quit
+        if keys[pygame.K_q] or event.type == pygame.QUIT:
+            pygame.display.quit()
+            break
--- planet_class.py
+++ planet_class.py
@@ -1,9 +1,26 @@
 import numpy as np
 import random as rand
 import pygame
+from functions import *
 
 size_mult = 10
 G = 1
+acc_mult = 200
+vel_mult = 0.01
+
+show_acc = False
+acc_vec_scale = 25
+
+show_vel = False
+vel_vec_scale = 0.3
+
+def toggle_acc ():
+    global show_acc
+    show_acc = not show_acc
+
+def toggle_vel ():
+    global show_vel
+    show_vel = not show_vel
 
 class planet:
     def __init__(self, mass, coords, vel, color):
@@ -16,16 +33,25 @@ class planet:
 
     def draw(self, surface, camera_pos):
         pygame.draw.ellipse(surface, self.color, pygame.Rect(self.coords[0] - camera_pos[0] - (self.diam/2), self.coords[1] - camera_pos[1] - (self.diam/2), self.diam, self.diam))
+        if show_acc and show_vel:
+            draw_arrow(surface, pygame.math.Vector2(self.coords[0] - camera_pos[0], self.coords[1] - camera_pos[1]), pygame.math.Vector2(np.add(self.vel * vel_vec_scale, self.coords)[0] - camera_pos[0], np.add(self.vel * vel_vec_scale, self.coords)[1] - camera_pos[1]), (0, 200, 0))
+            draw_arrow(surface, pygame.math.Vector2(self.coords[0] - camera_pos[0] + (self.vel * vel_vec_scale)[0], self.coords[1] - camera_pos[1] + (self.vel * vel_vec_scale)[1]), pygame.math.Vector2(np.add(self.acc * acc_vec_scale, self.coords)[0] - camera_pos[0] + (self.vel * vel_vec_scale)[0], np.add(self.acc * acc_vec_scale, self.coords)[1] - camera_pos[1] + (self.vel * vel_vec_scale)[1]), (200, 0, 0))
+        elif show_vel:
+            draw_arrow(surface, pygame.math.Vector2(self.coords[0] - camera_pos[0], self.coords[1] - camera_pos[1]), pygame.math.Vector2(np.add(self.vel * vel_vec_scale, self.coords)[0] - camera_pos[0], np.add(self.vel * vel_vec_scale, self.coords)[1] - camera_pos[1]), (0, 200, 0))
+        elif show_acc:
+            draw_arrow(surface, pygame.math.Vector2(self.coords[0] - camera_pos[0], self.coords[1] - camera_pos[1]), pygame.math.Vector2(np.add(self.acc * acc_vec_scale, self.coords)[0] - camera_pos[0], np.add(self.acc * acc_vec_scale, self.coords)[1] - camera_pos[1]), (200, 0, 0))
 
     def force(self, planets, to_delete):
+        self.acc = np.array([0, 0])
         for i in planets:
             if i != self:
                 force_dir = i.coords - self.coords
                 r = np.linalg.norm(force_dir)
-                self.acc = np.add(G * i.mass * force_dir / (r ** 3), self.acc)
+                self.acc = np.add(G * i.mass * force_dir / (r ** 3) * acc_mult, self.acc)
                 if r < (self.diam + i.diam) /2 and len(to_delete) == 0:
                     to_delete.append(self)
                     to_delete.append(i)
 
     def up_and_move (self):
-        pass
+        self.vel = np.add(self.vel, self.acc)
+        self.coords = np.add(self.coords, self.vel * vel_mult)
